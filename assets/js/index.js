bab2bb30caa92a76ec93abeb012998e781966aca
katampoi <simonkatampoi@student.moringaschool.com>
2022-06-23 07:22:09 +0300

    added our food section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 //fetch(url,options);(option contains headers,methods,Body)
-document.addEventListener("DOMContentLoaded",() =>{
-    const resturantDetailsUrl = "https://api.example.com/restaurants/:id"
+// document.addEventListener("DOMContentLoaded",() =>{
+//     const resturantDetailsUrl = "https://api.example.com/restaurants/:id"
 
 //     function getProject(){
 //        let reqoptions ={
@@ -19,13 +19,26 @@ document.addEventListener("DOMContentLoaded",() =>{
 
 // })
 
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
+// var requestOptions = {
+//     method: 'GET',
+//     redirect: 'follow'
+//   };
   
-  fetch("https://api.example.com/meals/?restaurantId=2", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-})
+//   fetch("https://api.example.com/meals/?restaurantId=2", requestOptions)
+//     .then(response => response.text())
+//     .then(result => console.log(result))
+//     .catch(error => console.log('error', error));
+// })
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '021a3814f1mshc051263799b9e92p14093ejsn3339dc83df32',
+		'X-RapidAPI-Host': 'udayogra-indian-restaurants-menu-v1.p.rapidapi.com'
+	}
+};
+
+fetch('https://udayogra-indian-restaurants-menu-v1.p.rapidapi.com/dl?area=koramangala&city=bangalore', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err))
