73657320e95d6fa1f46ac1d8c5addab5df9393fd
katampoi <simonkatampoi@student.moringaschool.com>
2022-06-23 15:51:05 +0300

    added the form javascript functionnality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,44 +1,25 @@
 //fetch(url,options);(option contains headers,methods,Body)
-// document.addEventListener("DOMContentLoaded",() =>{
-//     const resturantDetailsUrl = "https://api.example.com/restaurants/:id"
+const form = document.querySelector("#form");
 
-//     function getProject(){
-//        let reqoptions ={
-//         method:"GET"
-//        } 
-//        fetch(resturantDetailsUrl, reqoptions)
-//        .then((response)=>(response.json()))
-//        .then(result =>{
-//         console.log(result)
-//        })
+const firstNAMEREQUIRED = "Please enter your name";
+const lastNAMEREQUIRED = "Please enter your name";
+const EMAIL_REQUIRED = "Please enter your email";
+const EMAIL_INVALID = "Please enter a correct email address format";
+form.addEventListener("submit", function (event) {
+    const fname = event.target.fname.value
+    const lName = event.target.lname.value
+    const email = event.target.email.value
+    const message = event.target.message.value
+
+	// stop form submission
+	// event.preventDefault();
+
+	// validate the form
+	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
+	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
+	// if valid, submit the form.
+    form.submit()
+});
 
 
 
-//        getProject()
-//     }
-
-// })
-
-// var requestOptions = {
-//     method: 'GET',
-//     redirect: 'follow'
-//   };
-  
-//   fetch("https://api.example.com/meals/?restaurantId=2", requestOptions)
-//     .then(response => response.text())
-//     .then(result => console.log(result))
-//     .catch(error => console.log('error', error));
-// })
-
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '021a3814f1mshc051263799b9e92p14093ejsn3339dc83df32',
-		'X-RapidAPI-Host': 'udayogra-indian-restaurants-menu-v1.p.rapidapi.com'
-	}
-};
-
-fetch('https://udayogra-indian-restaurants-menu-v1.p.rapidapi.com/dl?area=koramangala&city=bangalore', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err))
